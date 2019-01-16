c4354313f0415104f99cd3e11a69f2bb873fce9a
Ravi Shekhar Jethani <rsjethani@gmail.com>
2019-01-17 02:25:30 +0530

    Change default monitoring interval to 5min
    Also refactor main.go to be simpler

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"time"
 
@@ -18,43 +17,50 @@ var argNormalInterval time.Duration
 func init() {
 	flag.BoolVar(&argWatch, "w", false, "continously watch battery status")
 	flag.DurationVar(&argLowInterval, "l", time.Minute*2, "battery check interval during low battery")
-	flag.DurationVar(&argNormalInterval, "n", time.Minute*10, "battery check interval during good battery")
+	flag.DurationVar(&argNormalInterval, "n", time.Minute*5, "battery check interval during good battery")
 	flag.UintVar(&argThreshold, "t", 20, "threshold below which battery capacty would be considered critical")
+}
+
+func getBatteryStatus() (uint, string, error) {
+	info, err := sysinfo.GetInfo("hardware", "battery")
+	if err != nil {
+		return 0, "", err
+	}
+	c, _ := info.Attribute(0, "CAPACITY")
+	capacity, _ := c.(uint)
+	s, _ := info.Attribute(0, "STATUS")
+	state, _ := s.(string)
+
+	return capacity, state, nil
 }
 
 func main() {
 	flag.Parse()
 
 	for {
-
-		info, err := sysinfo.GetInfo("hardware", "battery")
+		capacity, state, err := getBatteryStatus()
 
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		c, _ := info.Attribute(0, "CAPACITY")
-		capacity, _ := c.(uint)
-		s, _ := info.Attribute(0, "STATUS")
-		state, _ := s.(string)
+		fmt.Println(capacity, state)
 
-		log.Println(capacity, state)
-
-		if argWatch {
-			sleepInterval := argNormalInterval
-			if state == "Discharging" && capacity < argThreshold {
-				sleepInterval = argLowInterval
-				err = sendNotification(capacity, state)
-				if err != nil {
-					fmt.Println("an error on displaying notification occured: %s", err)
-					os.Exit(2)
-				}
-			}
-			time.Sleep(sleepInterval)
-			continue
+		// If -w not given, exit
+		if !argWatch {
+			break
 		}
 
-		break
+		sleepInterval := argNormalInterval
+		if state == "Discharging" && capacity < argThreshold {
+			sleepInterval = argLowInterval
+			err = sendNotification(capacity, state)
+			if err != nil {
+				fmt.Println("an error on displaying notification occured: %s", err)
+				os.Exit(2)
+			}
+		}
+		time.Sleep(sleepInterval)
 	}
 }
